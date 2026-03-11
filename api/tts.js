6f35f8f6b854806ff58cb270e85a69c26b6c6e71
The Human Side of Business <thusbusiness@MacBook-Air.local>
2026-03-11 14:53:49 -0600

    switch to faster tts model

--- api/tts.js
+++ api/tts.js
@@ -17,7 +17,7 @@ export default async function handler(req, res) {
         Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
       },
       body: JSON.stringify({
-        model: 'tts-1-hd',
+        model: 'tts-1',
         voice: 'echo',
         input: text,
         speed: 1.0,
